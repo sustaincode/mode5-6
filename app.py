6ae93c427a6040a20b655b4c3ec5a30b54ef30ef
Mathavan Arugalaimuthu <mathavan@aout.io>
2025-05-13 13:28:03 +0000

    4

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 #gemini
-
-from flask import Flask, request,render_template
-
-app =Flask(__name__)
-
-@app.route("/", methods =["GET", "POST"])
+from flask import Flask,request,render_template
+app = Flask(__name__)
+@app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
+
+@app.route("/gemini",methods=["GET","POST"])
+def gemini():
+    return(render_template("gemini.html"))
+
+@app.route("/gemini_reply",methods=["GET","POST"])
+def gemini_reply():
+    q = request.form.get("q")
+    print(q)
+    #gemini
+    r=q
+    return(render_template("gemini_reply.html",r=r))
+
 
 if __name__ == "__main__":
     app.run()
