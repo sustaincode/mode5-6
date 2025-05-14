d3caff50d0feb9ff857f9aedb12a8228ff48ca28
Mathavan Arugalaimuthu <mathavan@aout.io>
2025-05-14 11:20:11 +0000

    7

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask,request,render_template
 import google.generativeai as genai
 import os
+
 gemini_api_key = os.getenv("gemini_api_key")
 genai.configure(api_key=gemini_api_key)
 model = genai.GenerativeModel("gemini-2.0-flash")
+
 app = Flask(__name__)
+
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
